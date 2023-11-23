f3c1843cfbb65674edd3ea0adbd3e05981c3a00d
jaquemoura <49768265+jaquemoura@users.noreply.github.com>
2023-11-23 17:14:37 -0300

    home.vue

---
+++ home.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <btn />    
+    </main>
+</template>
+
+
+<script>
+
+
+export default {
+    components: {
+        btn: () => import(/* webpackChunkName: "home" */ "../btnSearch.vue")
+    },
+};
+</script>
